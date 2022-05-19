d0e86a069184503a577acad4ca4156855cf4e3a0
JunoField <junofield123@gmail.com>
2022-05-19 16:08:45 +0100

    done accordion besides inverting arrow

--- dist/index.1cc3346d.js
+++ dist/index.1cc3346d.js
@@ -1,8 +1,17 @@
+const noOfAnswers = 5;
+//input 0 for close *all*
+function closeAllOthers(desiredPair, noOfAnswers1) {
+    for(let i = 1; i <= noOfAnswers1; i++)if (i != desiredPair) {
+        document.getElementById("question-" + i).classList.remove("question-open");
+        document.getElementById("answer-" + i).classList.remove("answer-open");
+    }
+}
 function processClick(qaNo) {
     if (document.getElementById("question-" + qaNo).classList.contains("question-open")) {
         document.getElementById("question-" + qaNo).classList.remove("question-open");
         document.getElementById("answer-" + qaNo).classList.remove("answer-open");
     } else {
+        closeAllOthers(qaNo, noOfAnswers); //close all open qa pairs
         document.getElementById("question-" + qaNo).classList.add("question-open");
         document.getElementById("answer-" + qaNo).classList.add("answer-open");
     }
